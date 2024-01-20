cc667aea57aef2ef4dd3ee823909bb526647a47c
Aakanksha Hande <handeaakanksha@gmail.com>
2024-01-20 13:43:42 +0530

    Adding all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import './pages/main.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <><Navbar /><Header /></>
+
   );
 }
-
-export default App;
+  export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div>
+      <div id="carouselExampleFade" class="carousel slide carousel-fade">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <img src="C:\Users\Admin\Desktop\janseva-portal\src\Images\worker-from-dressed-work-clothes-observing-some-excavators-construction-site-ai-generative_123827-23542.avif" class="d-block w-100" alt="Roads"/> 
+    </div>
+    
+    <div class="carousel-item">
+      <img src="C:\Users\Admin\Desktop\janseva-portal\src\Images\sun-setting-silhouette-electricity-pylons_1127-3239.avif" class="d-block w-100" alt="Electricity"/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+    </div>
+  )
+}
+
+export default Header
+
