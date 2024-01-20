956d20f5388fb7f5ddbec41284b02447c08a5281
Aakanksha Hande <handeaakanksha@gmail.com>
2024-01-20 14:04:48 +0530

    Change in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,17 +19,7 @@ const Navbar = () => {
         <li class="nav-item">
           <a class="nav-link" href="#">Login</a>
         </li>
-        {/* <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-           
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li> */}
+        
         <li class="nav-item">
           <a class="nav-link " href='#'>Admin Login</a>
         </li>
